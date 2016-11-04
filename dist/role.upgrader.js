579bc4f4f3cf5156ce335e8aa22dd62ce17c2e92
Or Gerson <or.gerson@hpe.com>
2016-11-04 20:03:42 +0200

    base files

---
+++ dist/role.upgrader.js
@@ -0,0 +1,29 @@
+var roleUpgrader = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+        var prevPos = creep.memory.prevPos;
+        creep.memory.prevPos = creep.pos;
+        if (prevPos == creep.pos)
+        {
+            creep.memory.currentPath = null;
+        }
+
+        if(creep.carry.energy < creep.carryCapacity) {
+            var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[1]);
+            }
+        }
+        else if (creep.carry.energy == creep.carryCapacity) {
+            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                if (typeof variable == 'undefined') {
+                    creep.memory.currentPath = creep.findPath(creep.room.controller);
+                }
+                creep.moveByPath(creep.memory.currentPath);
+            }
+        }
+    }
+};
+
+module.exports = roleUpgrader;
