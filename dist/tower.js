9aabf56c5db8fdc8ddf73532f0791389c9c9dc1a
Or Gerson <or.gerson@hpe.com>
2016-11-16 17:51:15 +0200

    commented tower repair code

--- dist/tower.js
+++ dist/tower.js
@@ -3,12 +3,12 @@ var tower = {
 
     searchAndAttack: function(tower) {
         if(tower) {
-            var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+            /*var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
                 filter: (structure) => structure.hits < structure.hitsMax
             });
             if(closestDamagedStructure) {
                 tower.repair(closestDamagedStructure);
-            }
+            }*/
 
             var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
             if(closestHostile) {
