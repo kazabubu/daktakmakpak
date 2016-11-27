a80ba3e31371129de97b2c287869d370d259d90c
kazabubu <or.gerson@gmail.com>
2016-11-27 23:25:53 +0200

    auto spawn fix

--- dist/autoSpawn.js
+++ dist/autoSpawn.js
@@ -10,7 +10,8 @@ class AutoSpawn {
 
     spawn(){
         if (Game.rooms[ROOM].energyAvailable >= 1650 && (!_.isUndefined(Memory.dyingCount) && Memory.dyingCount[ROOM].count < 3 )){
-            for(var upgrader in upgraders){
+            for(var upgraderIndex in upgraders){
+                var upgrader = upgraders[upgraderIndex];
                 if((_.isUndefined(Game.creeps[upgrader]) || Game.creeps[upgrader].ticksToLive <= 0) && Game.rooms[ROOM].energyAvailable >= 1650) {
                     Game.spawns['Spawn1'].createCreep([WORK,WORK,WORK,WORK,WORK,WORK,WORK,WORK,CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE,MOVE]
                         ,upgrader,{role:'upgrader'});
